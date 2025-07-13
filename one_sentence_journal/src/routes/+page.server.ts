d8404696b9b75d0ace72ee492c623cb9f2fc06c9
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2025-07-13 16:09:46 +0200

    date formatting added

--- one_sentence_journal/src/routes/+page.server.ts
+++ one_sentence_journal/src/routes/+page.server.ts
@@ -1,9 +1,17 @@
 import { query } from '$lib/db';
 import type { ServerLoad } from '@sveltejs/kit';
 
+type Sentence = {
+  date: string;
+  text: string;
+};
+
 export const load: ServerLoad = async () => {
-	const res = await query(`SELECT date, text FROM sentences ORDER BY date DESC`);
-	return {
-		sentences: res.rows
-	};
+  const res = await query('SELECT date, text FROM sentences ORDER BY date DESC');
+  const sentences: Sentence[] = res.rows.map(row => ({
+    date: row.date instanceof Date ? row.date.toISOString().slice(0, 10) : row.date,
+    text: row.text
+  }));
+
+  return { sentences };
 };
